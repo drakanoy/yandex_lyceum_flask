04b9dbe8cf859701232d9c0a99b7d965fd6121f4
drakanoy <drakanoy@gmail.com>
2021-03-26 21:46:28 +0300

    картинка. задача 2

--- main.py
+++ main.py
@@ -22,5 +22,14 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/training/<prof>')
+def prof(prof):
+    param = {}
+    param['prof'] = prof
+    param['img1'] = url_for('static', filename='img/cor1.png')
+    param['img2'] = url_for('static', filename='img/cor2.png')
+    return render_template('prof.html', **param)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
