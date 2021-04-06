a3c4fb9dde365dfa03e6241f561b512cb75e40b2
drakanoy <drakanoy@gmail.com>
2021-04-06 23:14:32 +0300

    фласк скл алхимия учебник. html

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
 from data import db_session
 from data.users import User
+from data.news import News
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,6 +11,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/blogs.db")
     app.run()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 if __name__ == '__main__':
